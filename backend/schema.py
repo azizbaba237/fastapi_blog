1851b081ae5c7fcb28cab809c2a8de8f6a7eecaa
azizbaba237 <abdoulazizbaba227@gmail.com>
2026-07-27 18:09:54 +0100

    debut de configuration jwt, connexion gestions de token etc

--- backend/schema.py
+++ backend/schema.py
@@ -1,27 +1,34 @@
 from pydantic import BaseModel, ConfigDict, Field, EmailStr
 from datetime import datetime 
 
-
 # Base class for user data
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email : EmailStr = Field(max_length=120)
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length= 8)
 
 class UserUpdate(BaseModel):
     username: str | None = Field(min_length=1, max_length=50, default=None)
     email: EmailStr | None = Field(max_length=100, default=None)
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    id: int 
+    
+    id: int
+    username:str 
     image_file: str | None 
     image_path: str 
+    
+class UserPrivate(UserPublic):
+    email: EmailStr
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 # Base class for post data
 class PostBase(BaseModel):
@@ -42,6 +49,6 @@ class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id: int 
     user_id: int
-    author: UserResponse
+    author: UserPublic
     date_posted: datetime
     
